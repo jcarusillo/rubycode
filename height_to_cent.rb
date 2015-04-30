e78ed2b8186ee22872898bac5a1fd8105bf4e802
jcarusillo <jlcarusillo@gmail.com>
2015-04-30 00:06:39 +0000

    madlibs

---
+++ height_to_cent.rb
@@ -0,0 +1,9 @@
+puts "Hello! What is your height in inches?"
+x = gets.chomp
+y = x.to_i * 2.54
+puts "Congrats! You are " + y .to_s + " centimeters tall."
+puts "How much do you weigh in pounds?"
+x = gets.comp
+y = x.to_i * 0.45.to_i
+puts "Congrats! you are " + y.to_s + "killograms."
+
---
+++ mad_lib.rb
@@ -0,0 +1,26 @@
+puts "Tell me your name."
+name = gets.chomp
+puts "Hello" + name. "We are going to create a Driving tips mad lib."
+puts "Tell me an adjective."
+adjective1 = gets.chomp
+puts "Tell me an adjective."
+adjective2 = gets.chomp
+puts "Tell me an adjective."
+adjective3 = gets.chomp
+puts "Tell me an adverb."
+adverb = gets.chomp
+puts "Tell me a noun."
+noun1 = gets.chomp
+puts "Tell me a noun."
+noun2 = gets.chomp
+puts "Tell me a noun."
+noun3 = gets.chomp
+puts "Tell me a noun."
+noun4 = gets.chomp
+puts "Tell me a noun."
+noun5 = gets.chomp
+puts = "Tell me a noun."
+noun6 = gets.chomp
+puts = "Tell me a plural noun."
+plural = gets.chomp
+puts "Here is your mad lib! Driving a car can be fun if you follow this" + adjective1 + " advice: When approaching a" + noun1 + "on the right, always blow your" + noun2 + " . Before making an" + adjective2 + " turn, always stick your" + noun3 + " out the window. Every 2000 miles have your" + noun4 + " inspected and your" + noun5 " checked. When approaching your school, watch out for your" + adjective3 + plural + " . Above all, drive" + adverb + " . The" + noun6 + " you save may be your own."
